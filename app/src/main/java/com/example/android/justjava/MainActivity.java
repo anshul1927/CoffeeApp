0e0086d9848d6cbf25f10755b2e40bb4a8af58be
gopalgoel19 <mailgopalhere@gmail.com>
2017-09-06 12:49:06 +0530

    Make view scrollable. App now works in landscape mode

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,12 +1,12 @@
 package com.example.android.justjava;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
     int quantity=1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
