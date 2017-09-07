525fa6f65715d04b57e66c9d7aa5b186ae841dbe
gopalgoel19 <mailgopalhere@gmail.com>
2017-09-07 18:15:46 +0530

    Add gmail intent in the app.

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.android.justjava;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
+
+import static com.example.android.justjava.R.drawable.coffee;
 
 public class MainActivity extends AppCompatActivity {
     int quantity=1;
@@ -26,10 +30,18 @@ public class MainActivity extends AppCompatActivity {
         Integer price = calculatePrice(quantity);
         displayMessage(price.toString()+"$");
     }
-    public void submitOrder(View view) {
-        displayMessage("Order Placed");
+    public void submitOrder(View view){
+        String orderMessage = "Get me " + quantity + " cups of coffee." ;
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:cofe@gmail.com"));
+        intent.putExtra(Intent.EXTRA_EMAIL, "coffee@gmail.com");
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Coffee Order");
+        intent.putExtra(Intent.EXTRA_TEXT, orderMessage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
-    private Integer calculatePrice(int quantity) {
+    private Integer calculatePrice(int quantity){
         Integer price = quantity * 5;
         return price;
     }
